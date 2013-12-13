380ec00b0a10a5ea8e9d123ea8a3a5e3297d22b1
omar-ahmed <omar.solve@gmail.com>
2013-12-13 09:40:54 +0200

    add belt class to the project

---
+++ src/Belt.java
@@ -0,0 +1,8 @@
+
+public class Belt {
+	// belt is the line which the plates walks on and fall off it 
+	int end;
+	public Belt(int end) {
+		this.end = end;
+	}
+}
--- src/GameModel.java
+++ src/GameModel.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+
 
 public class GameModel {
 	Player player1;
 	Player player2;
+	Belt rightBelt;
+	Belt leftBelt;
+	ArrayList<Shape> plates;
+	
 	
 }
