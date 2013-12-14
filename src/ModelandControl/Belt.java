2d953440a39ff7f527d72715b37ee28f3d1465f0
omar-ahmed <omar.solve@gmail.com>
2013-12-14 09:22:19 +0200

    adding getters and setters to belt

--- src/ModelandControl/Belt.java
+++ src/ModelandControl/Belt.java
@@ -3,8 +3,14 @@ package ModelandControl;
 public class Belt {
 	// belt is the line which the plates walks on and fall off it 
 	int start, end;
+	int number_of_the_paltes_on_the_belt;
 	public Belt(int start, int end) {
 		this.start = start;
 		this.end   = end;
+		number_of_the_paltes_on_the_belt = 0;
 	}
+	
+	public int getPlatesNumber() {return number_of_the_paltes_on_the_belt;}
+	public void setPlatesNumber(int plates) {number_of_the_paltes_on_the_belt = plates;}
+	
 }
